4421da51875023ea5c7f9acde6680b92f89de1e4
Mehdi Naji <snaji001@ucr.edu>
2024-01-18 14:11:23 -0800

    Initial commit

---
+++ R/dd.R
@@ -0,0 +1,10 @@
+#' Title
+#'
+#' @format A data frame with 3 variables:
+#' \describe{
+#'   \item{YearMonth}{Description of var1}
+#'   \item{Year}{Description of var2}
+#'   \item{Month}{Description of var3}
+#'   \item{GEO}{Description of var3}
+#' }
+"dd"
